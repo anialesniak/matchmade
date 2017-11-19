bb67939f107fe904380833fd957fc0deec962823
Kuba Gwizdała <jakub.gwizdala@gmail.com>
2017-11-19 13:37:31 +0100

    ClientPool simple static interface

--- src/main/java/matchmaker/ClientPool.java
+++ src/main/java/matchmaker/ClientPool.java
@@ -7,22 +7,47 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public final class ClientPool
 {
-    private static final ClientPool INSTANCE = new ClientPool();
+    private static ClientPool INSTANCE = createEmptyClientPool();
 
-    private final Set<Client> clientSet;
+    private Set<Client> clientSet;
+
+    private static ClientPool createEmptyClientPool()
+    {
+        return new ClientPool();
+    }
 
     private ClientPool()
     {
         clientSet = ConcurrentHashMap.newKeySet();
     }
 
-    public static ClientPool getInstance()
+    private static ClientPool getInstance()
     {
         return INSTANCE;
     }
 
-    public Set<Client> getClientSet()
+    private Set<Client> getClientSet()
     {
         return clientSet;
+    }
+
+    public static void add(Client client)
+    {
+        INSTANCE.getClientSet().add(client);
+    }
+
+    public static boolean contains(Client client)
+    {
+        return INSTANCE.getClientSet().contains(client);
+    }
+
+    public static boolean remove(Client client)
+    {
+        return INSTANCE.getClientSet().remove(client);
+    }
+
+    public static void clear()
+    {
+        INSTANCE = createEmptyClientPool();
     }
 }
