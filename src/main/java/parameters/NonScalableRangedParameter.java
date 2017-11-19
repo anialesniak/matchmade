de961ac210c41805c41d3a40b4867c36fa096ea2
annterina <anna.julia.lesniak@gmail.com>
2017-11-19 19:45:09 +0100

    After review comments

--- src/main/java/parameters/NonScalableRangedParameter.java
+++ src/main/java/parameters/NonScalableRangedParameter.java
@@ -1,8 +1,5 @@
 package parameters;
 
-
-import com.google.common.collect.Range;
-
 public final class NonScalableRangedParameter extends RangedParameter implements NonScalable
 {
     public NonScalableRangedParameter(double lower, double upper)
