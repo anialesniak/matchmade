9b86fefeb73df2eaaaeca6b5ee0d24447fd71c74
Lukasz Sciga <lsciga@akamai.com>
2018-01-10 21:21:11 +0100

    forgot to add actual rule........

---
+++ src/integration-test/java/rules/ClientEnrollmentTestRule.java
@@ -0,0 +1,54 @@
+package rules;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.junit.rules.ExternalResource;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ClientEnrollmentTestRule extends ExternalResource
+{
+    private final String host;
+    private final int port;
+
+    public ClientEnrollmentTestRule(final String host, final int port)
+    {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void enrollClient(final String filePath)
+    {
+        final String body = readFile(filePath);
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), body);
+        Request request = new Request.Builder()
+                .url(String.format("http://%s:%d", host, port))
+                .post(requestBody)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readFile(final String filePath)
+    {
+        byte[] encoded = new byte[0];
+        try {
+            encoded = Files.readAllBytes(Paths.get(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String(encoded, Charset.defaultCharset());
+    }
+
+
+}
